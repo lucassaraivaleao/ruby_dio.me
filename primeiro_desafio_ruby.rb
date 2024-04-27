00a067c3df750c32fe12bd9697722497073fa95a
Lucas Leão <saraivaleaolucas@gmail.com>
2024-04-27 20:13:12 -0300

    primeiro desagio ruby

---
+++ primeiro_desafio_ruby.rb
@@ -0,0 +1,16 @@
+#Criar um programa que dê a saída do nome completo (nome e sobrenome) do usuário e sua idade
+
+print "Informe seu primeiro nome: "
+
+nome = gets.to_s
+
+print "Informe agora seu sobrenome: "
+
+sobrenome = gets.to_s
+
+print "E por fim, informe sua idade: "
+
+idade = gets.chomp.to_i
+
+
+print "Meu nome completo é "+nome+" "+sobrenome+" e tenho "+idade.to_s+" anos"""
